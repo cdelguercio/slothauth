24a5a4a0aaeb672ee54e20851f50a93f42ed60e5
Chris Del Guercio <cdelguercio@gmail.com>
2016-06-30 10:37:33 -0700

    added custom AccountForm

--- slothauth/views.py
+++ slothauth/views.py
@@ -112,11 +112,9 @@ class AccountViewSet(viewsets.GenericViewSet):
     serializer_class = AccountSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
-    @list_route(methods=['get', 'post', 'patch'])
+    @list_route(methods=['get', 'post', 'patch'], permission_classes=(permissions.IsAuthenticated, ))
     def me(self, request, *args, **kwargs):
 
-        if not request.user.is_authenticated():
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
         if request.method == 'PATCH':
             form = AccountForm(instance=request.user, data=request.data)
             if form.is_valid():
@@ -132,19 +130,22 @@ class AccountViewSet(viewsets.GenericViewSet):
     @list_route(methods=['post', 'patch'])
     def change_email(self, request, *args, **kwargs):
 
-        if 'email' in request.data and 'confirm_email' in request.data and 'password' in request.data:
-            if request.user.check_password(request.data['password']):
-                if request.data['email'].strip().lower() == request.data['confirm_email'].strip().lower():
-                    if re.match(r"[^@]+@[^@]+\.[^@]+", request.data['email']):
-                        request.user.email = request.data['email'].strip()
-                        request.user.save()
+        if 'email' not in request.data or 'confirm_email' not in request.data or 'password' not in request.data:
+            return Response({'error': 'EMAIL MISSING'}, status=status.HTTP_400_BAD_REQUEST)
 
-                        return Response(status=status.HTTP_204_NO_CONTENT)
-
-                    return Response({'error': 'EMAIL INVALID'}, status=status.HTTP_400_BAD_REQUEST)
-                return Response({'error': 'EMAIL MISMATCH'}, status=status.HTTP_400_BAD_REQUEST)
+        if not request.user.check_password(request.data['password']):
             return Response({'error': 'BAD PASSWORD'}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({'error': 'EMAIL MISSING'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not request.data['email'].strip().lower() == request.data['confirm_email'].strip().lower():
+            return Response({'error': 'EMAIL MISMATCH'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", request.data['email']):
+            return Response({'error': 'EMAIL INVALID'}, status=status.HTTP_400_BAD_REQUEST)
+
+        request.user.email = request.data['email'].strip()
+        request.user.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     ###
     # change_password
@@ -153,12 +154,26 @@ class AccountViewSet(viewsets.GenericViewSet):
     @list_route(methods=['post', 'patch'])
     def change_password(self, request, *args, **kwargs):
 
-        if 'current_password' not in request.data or 'password' not in request.data or 'password_repeat' not in request.data:
+        # Ensure that we are either testing the current_password, or the password_reset_key
+        if 'password' not in request.data or 'password_repeat' not in request.data or\
+           ('current_password' not in request.data and 'password_reset_key' not in request.data):
             return Response({'error': 'Missing fields'}, status=status.HTTP_400_BAD_REQUEST)
-        if not request.user.check_password(request.data['current_password']):
-            return Response({'error': 'Incorrect password'}, status=status.HTTP_400_BAD_REQUEST)
+
         if request.data['password'] != request.data['password_repeat']:
             return Response({'error': 'Password mismatch'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Test current_password
+        if 'current_password' in request.data and not request.user.check_password(request.data['current_password']):
+            return Response({'error': 'Incorrect password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Or test password_reset_key
+        if 'password_reset_key' in request.data and not request.data.get('password_reset_key') == request.user.password_reset_key:
+            return Response({'error': 'Incorrect password reset key'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # If the password_reset_key was attempted to be used, then reset it
+        if 'password_reset_key' in request.data and not request.data.get('password_reset_key') == '':
+            request.user.password_reset_key = Account._meta.get_field('password_reset_key').generate_unique(instance=request.user, sender=Account)
+
         request.user.set_password(request.data['password'])
         request.user.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
